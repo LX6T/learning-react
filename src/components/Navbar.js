b9a9a08540b0514077e18a3e9599ac3428a887f5
alex <alex@pullen.net.nz>
2022-05-08 17:39:10 +1200

    Fixed small logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
+import logo from '../images/reactjs-icon-small.png'
+
 export default function Navbar() {
     return (
         <nav>
-            <img src='../images/reactjs-icon-small.png' alt={'logo'} className={'nav--logo'}/>
+            <img src={logo} alt={'logo'} className={'nav--logo'}/>
             <h3 className={'nav--logo_text'}>ReactFacts</h3>
             <h4 className={'nav--title'}>React Course - Project 1</h4>
         </nav>
