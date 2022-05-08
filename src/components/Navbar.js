d542bdc2dc89d63277e640c64e8b840f60bbc2c4
alex <alex@pullen.net.nz>
2022-05-08 17:33:48 +1200

    Styled main section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
-import logo from '../images/logo.svg';
-
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} alt={'logo'} className={'nav--logo'}/>
+            <img src='../images/reactjs-icon-small.png' alt={'logo'} className={'nav--logo'}/>
             <h3 className={'nav--logo_text'}>ReactFacts</h3>
             <h4 className={'nav--title'}>React Course - Project 1</h4>
         </nav>
